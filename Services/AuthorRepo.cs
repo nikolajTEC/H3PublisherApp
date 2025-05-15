af3cfdeec21d6c4f5d32c4862ac3c1b2d4a69a60
nikolajTEC <nikolajstenberdtnielsen@gmail.com>
2025-05-15 10:35:05 +0200

    Add project files.

---
+++ Services/AuthorRepo.cs
@@ -0,0 +1,34 @@
+﻿
+using Core.DTO;
+using Newtonsoft.Json;
+using REST_API.Objects;
+
+namespace H3PublisherApp.Services;
+public class AuthorRepo
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public async Task<List<AuthorDTO>> GetAuthors()
+    {
+        try
+        {
+            // Define your API URL
+            string url = "https://localhost:7038/WeatherForecast/authors";
+
+            // Call the API
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            // Read and parse the response body
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<AuthorDTO> authors = JsonConvert.DeserializeObject<List<AuthorDTO>>(responseBody);
+
+            return authors;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return null;
+        }
+    }
+}
