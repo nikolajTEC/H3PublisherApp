d38debaea2e80377c7662d08e33aa5d50d295ffc
nikolajTEC <nikolajstenberdtnielsen@gmail.com>
2025-05-19 10:42:33 +0200

    start på frontend

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using H3PublisherApp.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddMudServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
